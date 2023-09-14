cb5a32afc762420189e5d852df1a3c0afb34cb84
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-09-14 21:59:17 +0300

    create basic MovieCardList component

--- src/components/MovieCardList/MovieCardList.js
+++ src/components/MovieCardList/MovieCardList.js
@@ -1,9 +1,20 @@
+// импорт компонент
+import MovieCard from '../MovieCard/MovieCard';
+
 // импорт стилей
 import './MovieCardList.css';
 
 // компонент списка карточек фильмов ////////////////////////////////////
 const MovieCardList = ({ movies }) => {
-  return <div>MovieCardList</div>;
+  return (
+    <section className="movies-list">
+      <ul className="movies-list__container">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 // экспорт //////////////////////////////////////////////////////
