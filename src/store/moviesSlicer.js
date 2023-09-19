494fe747e478f6cce935bbbfcb8928168741b006
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-09-19 18:36:09 +0300

    refactore fetchMovies & fetchFilteredMovies asyncThunks

--- src/store/moviesSlicer.js
+++ src/store/moviesSlicer.js
@@ -2,19 +2,23 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import movieApi from '../utils/MovieApi';
 
 export const fetchMovies = createAsyncThunk('movie/fetchMovies', async () => {
-  return await movieApi
-    .getAllMovies()
-    .then((movies) => movies)
-    .catch((error) => console.log(error));
+  try {
+    const movies = await movieApi.getAllMovies();
+    return movies;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export const fetchFilteredMovies = createAsyncThunk(
   'movie/fetchFilteredMovies',
   async (query) => {
-    return await movieApi
-      .getFilteredMovies(query)
-      .then((filteredMovies) => filteredMovies)
-      .catch((error) => console.log(error));
+    try {
+      const filteredMovies = await movieApi.getFilteredMovies(query);
+      return filteredMovies;
+    } catch (error) {
+      console.log(error);
+    }
   }
 );
 
