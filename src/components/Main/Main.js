1b87736792a3d25697f8cb52d13efebea63d2916
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-09-19 11:23:11 +0300

    shift fetchMovies from App to Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMovies } from '../../store/moviesSlicer';
+
 // импорт компонент
 import SearchForm from '../SearchForm/SearchForm';
 import MovieCardList from '../MovieCardList/MovieCardList';
@@ -7,6 +11,11 @@ import './Main.css';
 
 // компонент главной страницы с фильмами ////////////////////////////////////
 const Main = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
   return (
     <div className="movies">
       <main className="saved-movies__content">
