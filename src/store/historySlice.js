92582df9dc357ebd415c41c95e33886ab0e9c160
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-09-19 21:01:56 +0300

    create historySlice and add it to store

---
+++ src/store/historySlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const historySlice = createSlice({
+  name: 'history',
+  initialState: [],
+  reducers: {
+    addToHistory: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { addToHistory } = historySlice.actions;
+export default historySlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import user from './userSlice';
 import movies from './moviesSlice';
+import history from './historySlice';
 
 const store = configureStore({
-  reducer: { user, movies },
+  reducer: { user, movies, history },
 });
 
 export default store;
