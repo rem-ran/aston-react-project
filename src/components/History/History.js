8be1952d5e8282588015f971e99e93db7d7505a9
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-09-20 14:30:37 +0300

    add styles to History component

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 // импорт стилей
@@ -7,20 +7,26 @@ import './History.css';
 // компонент страницы с историей поисков ////////////////////////////////////
 const History = () => {
   const searchHistory = useSelector((state) => state.history);
+  const navigate = useNavigate();
 
   return (
     <div className="history">
       <main className="history__content">
-        <h2>История поиска фильмов</h2>
-        <ul>
+        <h2 className="history__heading">История поиска фильмов</h2>
+        <ul className="history__list">
           {searchHistory.map((query, index) => (
-            <li className="history__list-item" key={index}>
+            <li
+              className="history__list-item"
+              key={index}
+              onClick={() => navigate(`/search?q=${encodeURIComponent(query)}`)}
+            >
               <Link
                 className="history__link"
                 to={`/search?q=${encodeURIComponent(query)}`}
               >
                 {query}
               </Link>
+              <button className="history__delete-btn">Delete</button>
             </li>
           ))}
         </ul>
